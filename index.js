ccef0ad2d9a2e8e99178ce89b108f14953d1745b
Vinay R V <112092286+vinay-rv@users.noreply.github.com>
2022-10-10 22:23:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-function addRowBtm(){
-    let table = document.getElementById("mytbl");
-    let row=table.insertRow(6);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    cell1.innerHTML="6";
-    cell2.innerHTML="Rohit";
-    cell3.innerHTML="Sharma";
-    cell4.innerHTML="Mumbai";
-    cell5.innerHTML="India";
-    const button = document.querySelector('button')
-    button.disabled=true;
-    const button1 = document.getElementById("btn2");
-    button1.disabled=true;
-    document.querySelector("h2").innerHTML="Refresh and click on 'ADD ROW TO TOP' to add new row to the top";
-}
+var lst = document.getElementById("mytbl").rows.length;
+const fnames=["Rohit", "Virat","Vinay","Rocky","Rishab","Rakshith","Raj","KL"];
+const lnames=["Sharma","Kohli","RV","Bhai","Shetty","Shetty","Shetty","Rahul"];
+const city=["mumbai","Delhi","Bengaluru","Bengaluru","Mangolore","Mangolore","Mangolore","Tumkuru"]
+    let z=0;
 function addRowtop(){
     let table = document.getElementById("mytbl");
     let row=table.insertRow(1);
@@ -26,21 +12,36 @@ function addRowtop(){
     let cell4 = row.insertCell(3);
     let cell5 = row.insertCell(4);
     cell1.innerHTML="1";
-    cell2.innerHTML="Kl";
-    cell3.innerHTML="Rahul";
+    cell2.innerHTML=fnames[z];
+    cell3.innerHTML=lnames[z];
     cell4.innerHTML="Bengaluru";
     cell5.innerHTML="India";
-    var num=2;
-    for(var i=1;i<6;i++){
-        var x = document.getElementById(i);
-            x.innerHTML=num;
+    var num=1;
+    for(var i=1;i<=lst;i++){
+        var x = document.getElementById("mytbl").rows[i].cells;
+            x[0].innerHTML=num;
             num++;
         }
-        const button = document.querySelector('button')
-        button.disabled=true;
-        const button1 = document.getElementById("btn2");
-        button1.disabled=true;
-        document.querySelector("h2").innerHTML="Refresh and click on 'ADD ROW TO BOTTOM' to add new row to the bottom";            
+        lst++;
+        z++;
 
+}
+
+function addRowBtm(){
+    let table = document.getElementById("mytbl");
+    let row=table.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML= lst;
+    cell2.innerHTML=fnames[z];
+    cell3.innerHTML=lnames[z];
+    cell4.innerHTML="Mumbai";
+    cell5.innerHTML="India";
+    lst++;
+    z++;
+    
 }
         
